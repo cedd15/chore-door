1430bfaf365f3bf451390ded3269ef46d80104ed
cedd <cedricvincenth@yahoo.com>
2021-05-05 23:39:25 +0800

    added a function that assigns the chorebot to random doors

--- script.js
+++ script.js
@@ -6,14 +6,43 @@ const botDoorPath = "https://content.codecademy.com/projects/chore-door/images/r
 const beachDoorPath = "https://content.codecademy.com/projects/chore-door/images/beach.svg";
 const spaceDoorPath = "https://content.codecademy.com/projects/chore-door/images/space.svg";
 
+let numClosedDoors = 3;
+
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
+const randomChoreDoorGenerator = () => {
+    let choreDoor = Math.floor(Math.random() * numClosedDoors);
+    switch(choreDoor) {
+        case 0:
+            openDoor1 = botDoorPath;
+            openDoor2 = beachDoorPath;
+            openDoor3 = spaceDoorPath;
+            break;
+        case 1:
+            openDoor2 = botDoorPath;
+            openDoor1 = beachDoorPath;
+            openDoor3 = spaceDoorPath;
+            break;
+        case 2:
+            openDoor3 = botDoorPath;
+            openDoor1 = beachDoorPath;
+            openDoor2 = spaceDoorPath;
+            break;
+    }
+}
+
 doorImage1.onclick = () => {
-    doorImage1.src = botDoorPath;
+    doorImage1.src = openDoor1;
 }
 
 doorImage2.onclick = () => {
-    doorImage2.src = beachDoorPath;
+    doorImage2.src = openDoor2;
 }
 
 doorImage3.onclick = () => {
-    doorImage3.src = spaceDoorPath;
+    doorImage3.src = openDoor3;
 }
+
+randomChoreDoorGenerator();
